41a1c2cfb344a73ff3a16ad5904f977653763989
Jayleaf1125 <jasonpadilla104@gmail.com>
2022-03-03 00:18:52 -0500

    Group

--- navigate.js
+++ navigate.js
@@ -1,36 +1,44 @@
-// DOM Elements
-const randomBtn = document.querySelector(`#random-btn`);
-const selectBtn = document.querySelector(`#select-btn`);
-const welcomeSection = document.querySelector(`#welcome-section`);
-const filterForm = document.querySelector(`#filter-form`);
-const locationBtn = document.querySelector(`#location-btn`);
-const search = document.querySelector(`#search`);
-const locationForm = document.querySelector(`#location-form`);
-const groupDiv = document.querySelector(`#group-group`);
+// // DOM Elements
+// const randomBtn = document.querySelector(`#random-btn`);
+// const selectBtn = document.querySelector(`#select-btn`);
+// const welcomeSection = document.querySelector(`#welcome-section`);
+// const filterForm = document.querySelector(`#filter-form`);
+// const locationBtn = document.querySelector(`#location-btn`);
+// const search = document.querySelector(`#search`);
+// const locationForm = document.querySelector(`#location-form`);
+// const groupDiv = document.querySelector(`#group-group`);
 
-// Filter Form
-selectBtn.addEventListener('click', switchOverToFilterForm);
+// // Filter Form
+// selectBtn.addEventListener('click', switchOverToFilterForm);
 
-function switchOverToFilterForm() {
-    welcomeSection.style.display = "none";
-    locationForm.style.display = "grid";
-}
+// function switchOverToFilterForm() {
+//     welcomeSection.style.display = "none";
+//     locationForm.style.display = "grid";
+// }
 
-locationBtn.addEventListener('click', getBudgetValue);
+// locationBtn.addEventListener('click', getBudgetValue);
+
+// // function getBudgetValue() {
+// //     const value = budget.value;
+// //    console.log(+value)
+
+// //     filterForm.style.display = "none";
+
+// // }
 
 // function getBudgetValue() {
-//     const value = budget.value;
-//    console.log(+value)
+//     const value = search.value;
+//    console.log(value)
 
-//     filterForm.style.display = "none";
+//     locationForm.style.display = "none";
+//     groupDiv.style.display = "flex"
 
 // }
 
-function getBudgetValue() {
-    const value = search.value;
-   console.log(value)
-
-    locationForm.style.display = "none";
-    groupDiv.style.display = "flex"
-
-}
+fetch("https://pokeapi.co/api/v2/pokemon")
+.then(response => response.json()) 
+.then(data => {
+   data.results.forEach(element => {
+       console.log(element)
+   });
+})
